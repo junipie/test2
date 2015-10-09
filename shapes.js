be2b3882d8451c6e3328f766d61a81875df4612e
Juniper Chapman <juniper@wl-86-145.wireless.umt.edu>
2015-10-09 12:00:43 -0600

    finished the challenge, all tests successful

--- shapes.js
+++ shapes.js
@@ -1,27 +1,52 @@
-exports.Shape = function() {
-
+exports.Shape = function(name) {
+	this.name = name;
 }
 
-exports.Shape2d = function(){
-	
+exports.Shape2d = function(name, color){
+	exports.Shape.call(this, name);
+	this.color = color;
+	this.getColor = function(){
+		return color;
+	}
 }
-//Voodoo Magic to make Shape2d inherit from Shape
+exports.Shape2d.prototype = Object.create(exports.Shape.prototype);
+exports.Shape2d.prototype.constructor = exports.Shape2d;
+
 
 exports.Circle = function(name, color, radius){
-
+	exports.Shape2d.call(this, name, color);
+	this.radius = radius;
+	this.getRadius = function(){
+		return radius;
+	}
+	this.getDiameter = function(){
+		return 2*radius;
+	}
+	this.getArea = function(){
+		return Math.PI*(radius+radius);
+	}
 }
-//Voodoo Magic to make Circle inherit from Shape2d
+exports.Circle.prototype = Object.create(exports.Shape2d.prototype);
+exports.Circle.prototype.constructor = exports.Circle;
 
-exports.Shape3d = function(){
-	
+
+exports.Shape3d = function(name, color){
+	exports.Shape.call(this, name);
+	this.color = color;
+	this.getColor = function(){
+		return color;
+	}
 }
-//Voodoo Magic to make Shape3d inherit from Shape
+exports.Shape3d.prototype = Object.create(exports.Shape.prototype);
+exports.Shape3d.prototype.constructor = exports.Shape3d;
 
-exports.Cube = function(name, color, radius){
 
+exports.Cube = function(name, color, sideLength){
+	exports.Shape3d.call(this, name, color);
+	this.sideLength = sideLength;
+	this.getVolume = function (){
+		return sideLength*sideLength*sideLength;
+	}
 }
-//Voodoo Magic to make Cube inherit from Shape3d
-
-
-
-
+exports.Cube.prototype = Object.create(exports.Shape3d.prototype);
+exports.Cube.prototype.constructor = exports.Cube;
